48ab4b25d5503405eec359c262fb8ab234b93468
David Ansari <david.ansari@gmx.de>
2022-02-06 12:40:06 +0100

    Bump ginkgo to v2

--- client_test.go
+++ client_test.go
@@ -12,7 +12,7 @@ import (
 	"runtime"
 	"strings"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/ghttp"
 
--- errors_test.go
+++ errors_test.go
@@ -1,7 +1,7 @@
 package lastpass_test
 
 import (
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	. "github.com/ansd/lastpass-go"
